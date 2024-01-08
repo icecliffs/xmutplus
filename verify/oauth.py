cf811e557a7e24f666e1d3bac30d56cf10294bc6
IceCliffs <security@icecliffs.cn>
2024-01-08 19:58:28 +0800

    Update README.md

--- verify/oauth.py
+++ verify/oauth.py
@@ -1,9 +1,1 @@
-# window.consoleURL = {
-#     "loginOutUrl": "https://iam.xmut.edu.cn:443/idp//authCenter/GLO?redirectToLogin=true&redirectToUrl=https://iam.xmut.edu.cn:443",
-#     "ApiUrl": "https://iam.xmut.edu.cn:8083/",
-#     "loginUrl": "/admin-api/oauth/checklogin",
-#     "idpUrl": "https://iam.xmut.edu.cn:443/idp/",
-#     "title": "统一登录",
-#     "forgetPWDloginUrl": "https://iam.xmut.edu.cn:443/#/forgetPaswordIndex/forgetPassword"
-# }
-# https://i.xmut.edu.cn/cas/validate?sbsbsbsb&ticket=ST-306337-FaoNeMNpJYwXcMGPWJ--yLoTzgIbim01
+
